7f26d0975cdc3f1864b97517b4eac3f80da5366b
Tom(かっかきっきこ) <17a3134@sit.shonan-it.ac.jp>
2020-05-19 22:13:19 +0900

    fix serviceworker

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,2 @@
-var CACHE_NAME = "TFjs-HandPoseDemo-PWA";
-var urlsToCache = [
-    "/matsusato4.github.io/TFjs-HandPoseDemo/",
-];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-            })
-    );
-});
-
 self.addEventListener('fetch', function(e) {
 });
