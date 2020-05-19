632e0fbfcf722e2e2b6436470fe203f1b78e5fe2
Tom(かっかきっきこ) <17a3134@sit.shonan-it.ac.jp>
2020-05-20 00:06:10 +0900

    serviceworker with workbox

--- service_worker.js
+++ service_worker.js
@@ -1,25 +1,12 @@
-var CACHE_NAME = "TFjs-HandPoseDemo-PWA";
-var urlsToCache = [
-    "/index.html",
-    "index.js"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js')
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
-            })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '10000'
+  },
+  {
+    url: '/index.js',
+    revision: '10000'
+  },
+])
